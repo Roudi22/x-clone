8eb82fd1d4c7a68c0e46ef147c38c9a69452dda0
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-13 11:36:29 +0200

    add some middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
+app.use(express.json()); // to parse request body
+app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
