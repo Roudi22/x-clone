cb020ca7ffd103573e04a2861d62914aca5dd96e
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-14 12:51:34 +0200

    create the delete post function

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -25,3 +25,26 @@ export const createPost = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = req.user._id.toString();
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        if (post.user.toString() !== currentUserId) {
+            return res.status(401).json({ message: "Unauthorized to delete this post" });
+        }
+        if (post.image) {
+            const publicId = post.image.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(publicId);
+        }
+        await Post.findByIdAndDelete(id);
+        res.json({ message: "Post deleted successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
+};
