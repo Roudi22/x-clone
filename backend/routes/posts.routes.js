19ecdd4a6aa7c402f11da82c4cc3f2b636e5dfdc
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-14 17:15:43 +0200

    uncomment

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { commentOnPost, createPost, deletePost } from '../controllers/posts.controller.js';
+import { commentOnPost, createPost, deletePost, likeUnlikePost } from '../controllers/posts.controller.js';
 
 const router = express.Router();
 
 router.post("/create-post", protectRoute, createPost);
-// router.post("/like-post/:id", protectRoute, likeUnlikePost);
+router.post("/like-post/:id", protectRoute, likeUnlikePost);
 router.post("/comment-post/:id", protectRoute, commentOnPost);
 router.delete("/delete-post/:id", protectRoute, deletePost);
 
