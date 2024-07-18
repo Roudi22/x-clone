140be09af5132efc7d40d2c06542c88893670364
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-18 12:33:52 +0200

    add new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import LogInPage from "./pages/auth/login/LogInPage"
 import Home from "./pages/home/Home"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 
 function App() {
@@ -9,11 +13,15 @@ function App() {
 
   return (
     <div className='flex max-w-6xl mx-auto'>
+        <Sidebar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<LogInPage />} />
         <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notification' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
         </Routes>
+        <RightPanel/>
     </div>
   )
 }
