ee328ef05b150a486611d0a1c46b602b03b49031
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-18 22:05:30 +0200

    change the message to error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,107 +2,106 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 export const signup = async (req, res) => {
-    try {
-        const { username, fullName, password, email } = req.body;
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const existingUser = await User.findOne({ username });
-        const existingEmail = await User.findOne({ email });
-        if (existingEmail) {
-            return res.status(400).json({ message: "Email already taken" });
-        }
-        if (existingUser) {
-            return res.status(400).json({ message: "Username already taken" });
-        }
-        if (!emailRegex.test(email)) {
-            return res.status(400).json({ message: "Invalid Email" });
-        }
-        if (password.length < 6) {
-            return res.status(400).json({ message: "Password must be at least 6 characters" });
-        }
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new User({
-            username,
-            fullName,
-            email,
-            password: hashedPassword,
-        });
-        if (newUser) {
-            generateTokenAndSetCookie(newUser._id, res);
-            await newUser.save();
-            res.status(201).json({ message: "User created successfully",
-                user: {
-                    _id: newUser._id,
-                    username: newUser.username,
-                    fullName: newUser.fullName,
-                    email: newUser.email,
-                    followers: newUser.followers,
-                    following: newUser.following,
-                    profilePicture: newUser.profilePicture,
-                    coverPicture: newUser.coverPicture,
-                },
-            });
-        } else {
-            res.status(500).json({ message: "Failed to create user" });
-        }
-
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        res.status(500).json({ message: "Server Error" });
+  try {
+    const { username, fullName, password, email } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const existingUser = await User.findOne({ username });
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email already taken" });
     }
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already taken" });
+    }
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid Email" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters" });
+    }
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profilePicture: newUser.profilePicture,
+        coverPicture: newUser.coverPicture,
+      });
+    } else {
+      res.status(500).json({ message: "Failed to create user" });
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const login = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(400).json({ message: "Invalid username or password" });
-        }
-        const isMatch = await bcrypt.compare(password, user.password || "");
-        if (!isMatch) {
-            return res.status(400).json({ message: "Invalid username or password" });
-        }
-        generateTokenAndSetCookie(user._id, res);
-        res.status(200).json({ message: "Login successful",
-            user: {
-                _id: user._id,
-                username: user.username,
-                fullName: user.fullName,
-                email: user.email,
-                followers: user.followers,
-                following: user.following,
-                profilePicture: user.profilePicture,
-                coverPicture: user.coverPicture,
-            },
-        });
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        res.status(500).json({ message: "Server Error" });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
     }
+    const isMatch = await bcrypt.compare(password, user.password || "");
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        username: user.username,
+        fullName: user.fullName,
+        email: user.email,
+        followers: user.followers,
+        following: user.following,
+        profilePicture: user.profilePicture,
+        coverPicture: user.coverPicture,
+      },
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const logout = async (req, res) => {
-    try {
-        res.cookie("jwt", "", { maxAge: 0 });
-        res.status(200).json({ message: "Logout successful" });
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        res.status(500).json({ message: "Server Error " });
-        
-    }
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error " });
+  }
 };
 
 export const getMe = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id).select("-password");
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        res.status(500).json({ message: "Server Error in getMe controller" });
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error in getMe controller" });
+  }
 };
