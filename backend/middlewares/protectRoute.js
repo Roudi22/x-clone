bd8a75039dc1d71d5468abbe2dbcda4b94b2ed2e
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-19 13:57:53 +0200

    small fixes

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -4,17 +4,20 @@ export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized: No Token Provided" });
+            return res.status(401).json({ error: "Unauthorized: No Token Provided" });
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorized: Invalid Token" });
+        }
         const user = await User.findById(decoded.id).select("-password");
         if (!user) {
-            return res.status(401).json({ message: "User not found" });
+            return res.status(401).json({ error: "User not found" });
         }
         req.user = user;
         next();
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        res.status(401).json({ message: "Error from the protect route middleware" });
+        res.status(401).json({ error: "Error from the protect route middleware" });
     }
 };
