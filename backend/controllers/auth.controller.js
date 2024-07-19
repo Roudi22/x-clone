1f2dc2c35e010fb0f4abd45c7971f4d0ce934c66
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-19 13:57:34 +0200

    change the meesage to error in the response

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -89,7 +89,7 @@ export const logout = async (req, res) => {
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    res.status(500).json({ message: "Server Error " });
+    res.status(500).json({ error: "Server Error " });
   }
 };
 
@@ -97,11 +97,11 @@ export const getMe = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("-password");
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
     res.status(200).json(user);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    res.status(500).json({ message: "Server Error in getMe controller" });
+    res.status(500).json({ error: "Server Error in getMe controller" });
   }
 };
