459464af74678657da10b506a4ba8ac81c254963
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-21 10:25:08 +0200

    suggested users fetched

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 
+import { useQuery } from "@tanstack/react-query";
 const RightPanel = () => {
-    const isLoading = false;
+    
+	const { data:suggestedUsers, isLoading } = useQuery({
+		queryKey: ["suggestedUsers"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/users/suggested");
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.message);
+				}
+				console.log(data);
+				return data;
+			} catch (error) {
+				console.log(error);
+				throw new Error(error);
+			}
+		},
+	}); // Add the query key
   return (
     <div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
@@ -19,7 +36,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.users.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
