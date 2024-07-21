220b72df390b89fbb595f3d62868696c1628788e
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-21 10:14:34 +0200

    change the limit of the files size

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ cloudinary.config({
 
 const app = express();
 
-app.use(express.json()); // to parse request body
+app.use(express.json({limit:"5mb"})); // to parse request body
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser());
 
