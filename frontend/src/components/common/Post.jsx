ddf89964b9e268053c76435e48b45f1544c2beb1
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-23 19:34:50 +0200

    make the profile picture container squared

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -134,7 +134,7 @@ const Post = ({ post }) => {
 		<>
 			<div className='flex gap-2 items-start p-4 border-b border-gray-700'>
 				<div className='avatar'>
-					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
+					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full aspect-square overflow-hidden'>
 						<img src={postOwner.profileImg || "/avatar-placeholder.png"} />
 					</Link>
 				</div>
