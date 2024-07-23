246f862d51695b264025d9a7b98a4cd0efad05b7
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-23 19:56:37 +0200

    prepare to deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import path from 'path';
 import authRoutes from './routes/auth.routes.js';
 import usersRoutes from './routes/users.routes.js';
 import postsRoutes from './routes/posts.routes.js';
@@ -25,11 +25,16 @@ app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) => { res.sendFile (path.resolve(__dirname, 'frontend', 'dist', 'index.html')) });
+}
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
     connectMongoDB();
