7b73b9d7f812e94ba748fa5d23ff2d3d7e1f1cca
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-23 20:11:57 +0200

    fixes

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -180,7 +180,7 @@ export const getFollowingPosts = async (req, res) => {
       return res.status(200).json({posts: []});
     }
     
-    res.status(200).json({ counts: posts.length, posts });
+    res.status(200).json( posts );
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
